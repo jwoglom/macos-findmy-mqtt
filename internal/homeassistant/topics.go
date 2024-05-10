fc90de9139db32b42aff695b83e313c2b5938c02
James Woglom <j@wogloms.net>
2024-05-10 01:02:15 -0400

    support overriding mqtt topic

--- internal/homeassistant/topics.go
+++ internal/homeassistant/topics.go
@@ -1,11 +1,24 @@
 package homeassistant
 
-import "github.com/jwoglom/macos-findmy-mqtt/internal/fmipcore"
+import (
+	"flag"
 
-const BaseMqttTopic = "findmy-mqtt"
-const ConnectivityTopic = "homeassistant/binary_sensor/" + BaseMqttTopic + "/connectivity/config"
-const StatusTopic = BaseMqttTopic + "/status"
-const BaseDeviceTrackerTopic = "homeassistant/device_tracker/"
+	"github.com/jwoglom/macos-findmy-mqtt/internal/fmipcore"
+)
+
+var topic = flag.String("topic", "findmy-mqtt", "MQTT default topic name")
+
+var BaseMqttTopic string
+var ConnectivityTopic string
+var StatusTopic string
+var BaseDeviceTrackerTopic string
+
+func init() {
+	BaseMqttTopic = *topic
+	ConnectivityTopic = "homeassistant/binary_sensor/" + BaseMqttTopic + "/connectivity/config"
+	StatusTopic = BaseMqttTopic + "/status"
+	BaseDeviceTrackerTopic = "homeassistant/device_tracker/"
+}
 
 func ItemConfigTopic(item fmipcore.FmItem) string {
 	return BaseDeviceTrackerTopic + BaseMqttTopic + "_" + item.Identifier + "/config"
